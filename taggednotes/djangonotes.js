1228ea610a30703513fc9447b25422ac58be3104
Heitor Chang <heitorchang@gmail.com>
2019-10-28 12:38:43 -0300

    revive tagged notes for django

---
+++ taggednotes/djangonotes.js
@@ -0,0 +1,43 @@
+var notes = {
+note0: { 
+id: "note0", 
+title: "Hello Django",
+tags: ['tutorial'],
+reference: "28 oct 2019",
+body: "# Django Tutorial \n\
+ \n\
+* a \n\
+* b \n\
+* c \n\
+ \n\
+*hello* \n\
+" },
+
+note1: { 
+id: "note1", 
+title: "Virtualenv",
+tags: ['tutorial'],
+reference: "28 oct 2019",
+body: "# Virtualenv \n\
+ \n\
+``` \n\
+> source \n\
+ \n\
+``` \n\
+" },
+
+note2: { 
+id: "note2", 
+title: "Random HTML",
+tags: ['tutorial'],
+reference: "28 oct 2019",
+body: "# Random HTML \n\
+ \n\
+```html \n\
+<b>Bold</b> \n\
+``` \n\
+<b>Bold outside codeblock</b> \n\
+" },
+
+numNotes: 3
+};
---
+++ taggednotes/readnotes.js
@@ -0,0 +1,125 @@
+var tags = {};
+var availableNotes = [];
+
+function createNote(noteObj) {
+  var note = document.createElement("p");
+  
+  var title = document.createElement("p");
+
+  title.innerHTML = '<hr><span class="noteTitle" onclick="toggleNote(\'' + noteObj.id + '\')"><b>' + noteObj.title + '</b></span><br><br>';
+
+  var noteContents = document.createElement("p");
+  noteContents.setAttribute("id", noteObj.id);
+  noteContents.style.display = '';
+
+  var tags = document.createElement("span");
+
+  noteObj.tags.forEach(function(tag) {
+    tags.innerHTML += "[" + createTag(tag) + "] ";
+  });
+
+  tags.innerHTML += "<br><br>"
+
+  var ref = document.createElement("p");
+  ref.innerHTML = noteObj.reference;
+  ref.innerHTML += "<br><br>";
+  
+  var body = document.createElement("div");
+
+  body.innerHTML = marked(noteObj.body);
+
+  var padding = document.createElement("p");
+  padding.innerHTML = "<br><br>";
+  
+  noteContents.appendChild(tags);
+  noteContents.appendChild(ref);
+  noteContents.appendChild(body);
+  noteContents.appendChild(padding);
+  
+  note.appendChild(title);
+  note.appendChild(noteContents);
+  return note;
+}
+
+function createTag(tagName) {
+  return '<span onclick="createNotes(\'' + escape(tagName) + '\');">' + tagName + '</span>';
+}
+
+function createNotes(tagName) {
+  availableNotes = [];
+  var notesHTML = document.createElement("p");
+
+  tags[tagName].forEach(function(noteId) {
+    notesHTML.appendChild(createNote(notes[noteId]));
+    availableNotes.push(noteId);
+  });
+  
+  document.getElementById("notes").innerHTML = "<br>&nbsp;" + unescape(tagName) + ' <span onclick="expandAll();">(expand all)</span> <span onclick="collapseAll();">(collapse all)</span>';
+  document.getElementById("notes").appendChild(notesHTML);
+  document.getElementById("notes").scrollTop = 0;
+}
+
+function createAllNotes() {
+  availableNotes = [];
+  var notesHTML = document.createElement("p");
+
+  for (var i = 0; i < notes.numNotes; i++) {
+    noteId = "note" + i;
+    console.log(noteId);
+    notesHTML.appendChild(createNote(notes[noteId]));
+    availableNotes.push(noteId);
+  }
+  
+  document.getElementById("notes").innerHTML = '<br>All notes<span onclick="expandAll();"> (expand all)</span> <span onclick="collapseAll();">(collapse all)</span>';
+  document.getElementById("notes").appendChild(notesHTML);
+
+  collapseAll();
+  document.getElementById("notes").scrollTop = 0;
+}
+
+function expandAll() {
+  availableNotes.forEach(function(noteId) {
+    document.getElementById(noteId).style.display = '';
+  });
+}
+
+function collapseAll() {
+  availableNotes.forEach(function(noteId) {
+    document.getElementById(noteId).style.display = 'none';
+  });
+}
+
+function toggleNote(noteId) {
+  var note = document.getElementById(noteId);
+  if (note.style.display !== 'none') {
+    note.style.display = 'none';
+  } else {
+    note.style.display = '';
+  }
+}
+
+function collectTags(notesObj) {
+  var simpleTags = {};
+  
+  for (var i = 0; i < notesObj.numNotes; i++) {
+    var note = notesObj['note' + i.toString()];
+    note.tags.forEach(function(tag) {
+      var escapedTag = escape(tag);
+      if (!(escapedTag in tags)) {
+        tags[escapedTag] = [];
+        simpleTags[tag] = "";
+      }
+      tags[escapedTag].push(note.id);
+    });
+  }
+
+  var tagsHTML = '<br><span onclick="createAllNotes()">All notes</span><br><br>';
+
+  Object.keys(simpleTags).sort(function(a, b) {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+  }).forEach(function(tag) {
+    tagsHTML += "&nbsp;<span>" + createTag(tag) + "</span><br>";
+  });
+
+  return tagsHTML;
+}
