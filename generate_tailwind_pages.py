24f35c7f288e443f3746aafa72c2694286744b72
Heitor Chang <heitorchang@gmail.com>
2019-11-13 14:12:08 -0300

    dynamically generate projects

--- generate_tailwind_pages.py
+++ generate_tailwind_pages.py
@@ -1,14 +1,57 @@
+from collections import namedtuple
+
 TAILWIND_DIR = "tailwind/"
-CONTENT = ["index_tailwind.html", "home.html", "cv.html", "projects.html"]
+CONTENT = ["index_tailwind.html", "home.html", "cv.html"]
 
 HEADER = TAILWIND_DIR + "tailwind_navbar.html"
 FOOTER = TAILWIND_DIR + "tailwind_footer.html"
 
+Project = namedtuple("Project", "img title desc demo code")
+
+PROJECTS = [Project('PTL.png',
+                    "Pontual Import Brindes",
+                    "Catálogo de produtos",
+                    "http://pontualimportbrindes.com.br/",
+                    "https://github.com/pontual/kitweb-v2/"),
+            
+            Project('pangloss.png',
+                    'Pangloss',
+                    'Anotações sobre Python 3',
+                    "http://pangloss.surge.sh/",
+                    "https://github.com/heitorchang/learn-code/tree/master/pangloss"),
+            
+            Project('AllCents2.png',
+                    'All Cents',
+                    'App de contabilidade',
+                    "https://heitor.pythonanywhere.com/allcents/",
+                    "https://github.com/reddress/zeno/tree/master/benny"),
+            
+            Project('Sistema.png',
+                    'Sistema',
+                    'Gerenciamento de compra, vendas e estoque',
+                    "https://heitor.pythonanywhere.com/sistema/",
+                    "https://github.com/reddress/zeno/tree/master/sistema"),
+            
+            Project('Polylexis.png',
+                    'Polylexis',
+                    'Quiz de vocabulário grego e mandarim',
+                    "https://www.polylexis.com/",
+                    "https://github.com/heitorchang/polylexis"),
+]            
 
 if __name__ == "__main__":
+    # Index and CV
     for fname in CONTENT:
         with open(fname, 'w', encoding="utf-8") as fout:
             print(open(HEADER, encoding="utf-8").read(), file=fout)
             print(open(TAILWIND_DIR + fname, encoding="utf-8").read(), file=fout)
             print(open(FOOTER, encoding="utf-8").read(), file=fout)
                 
+    # Projects
+    with open("projects.html", 'w', encoding='utf-8') as fout:
+        print(open(HEADER, encoding="utf-8").read(), file=fout)
+        
+        for project in PROJECTS:
+            print(project.title, file=fout)
+
+        print(open(FOOTER, encoding="utf-8").read(), file=fout)
