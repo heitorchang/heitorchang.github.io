b8bf7d9318595624aa92e3f5bb00435a21fab308
Heitor Chang <heitorchang@gmail.com>
2020-01-06 17:14:21 -0300

    restore rest api

--- generate_resume.py
+++ generate_resume.py
@@ -23,8 +23,9 @@ class ResumeSection:
         
 pontual = Company("Pontual Exportação e Importação Ltda., São Paulo, SP",
                   [Job("Desenvolvedor Full-Stack",
-                       "Jan. 2014 &mdash; atual",
-                       ["Usar indicadores MA (Moving Average) em um projeto Django e Python para prever a demanda de produtos e calcular a quantidade adequada para reposições.",
+                       "Jan. 2014 &mdash; atual",                       
+                       ["Criar uma API REST com o framework Slim (em PHP) para ser consumida por clientes que desejam obter as características e estoque de mais de 600 produtos.",
+                        "Usar indicadores MA (Moving Average) em um projeto Django e Python para prever a demanda de produtos e calcular a quantidade adequada para reposições.",
                         "Visualizar históricos de vendas com NumPy e Pandas, obtendo médias e valores extremos para cada produto.",
                         "Desenvolver com PHP puro e MySQL o lado público e painel de administração do site, <a href=\"http://pontualimportbrindes.com.br/\" target=\"_blank\">http://pontualimportbrindes.com.br/</a>",
                         "Automatizar a coleta de dados do estoque atual com um robô escrito em Python, reformatando-os em uma página HTML para o uso de clientes.",
