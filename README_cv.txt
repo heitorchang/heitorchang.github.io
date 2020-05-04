f188bcda6ed109b9981c2a4d595cbfabdef36fb9
Heitor Chang <heitorchang@gmail.com>
2020-05-04 13:49:40 -0300

    english printable

--- README_cv.txt
+++ README_cv.txt
@@ -1,7 +1,14 @@
-To update resume:
+* To update Portuguese resume:
 
 Edit generate_resume.py
 
 Run (to generate printable version, add the 'print' argument)
 python generate_tailwind_pages.py [print]
 
+* To update English resume:
+
+Edit generate_resume_en.py
+
+Run python generate_resume_en.py
+
+The file cv_en_print.html will be created
---
+++ generate_resume_en.py
@@ -0,0 +1,299 @@
+# Generate printable version of English resume
+
+TAILWIND_DIR = "tailwind/"
+CV_HEADER = TAILWIND_DIR + "cv_header.html"
+
+class Job:
+    def __init__(self, title, dates, items):
+        self.title = title
+        self.dates = dates
+        self.items = items
+
+        
+class Company:
+    def __init__(self, name, jobs):
+        self.name = name
+        self.jobs = jobs
+
+
+class ResumeSection:
+    def __init__(self, name, companies):
+        self.name = name
+        self.companies = companies
+        
+        
+pontual = Company("Pontual Import Brindes, São Paulo, SP",
+                  [Job("Desenvolvedor Full-Stack",
+                       "Jan. 2014 &mdash; atual",                       
+                       ["Criar uma API REST com o framework Slim (em PHP) para ser consumida por um app React que gera o site público da empresa, <a href=\"http://pontualimportbrindes.com.br/\" target=\"_blank\">http://pontualimportbrindes.com.br/</a>",
+                        "Usar indicadores MA (Moving Average) em um projeto Django e Python para prever a demanda de produtos e calcular a quantidade adequada para reposições.",
+                        "Visualizar históricos de vendas com NumPy e Pandas, obtendo médias e valores extremos para cada produto.",
+                        "Desenvolver com PHP puro e MySQL o painel de administração do site.",
+                        "Automatizar a coleta de dados do estoque atual com um robô escrito em Python, reformatando-os em uma página HTML para o uso de clientes.",
+                        "Criar um SPA (aplicativo de página única) com AngularJS e Firebase para organizar listas de pedidos e reservas em tempo real."]),
+
+                   Job("Auxiliar Administrativo",
+                       "Nov. 2011 &mdash; Dez. 2013",
+                       ["Receber pedidos e reservas de clientes, formalizando-os na hora de retirada ou despacho.",
+                        "Fazer previsões de demanda com Excel para cada produto, determinando a quantidade e frequência de reposição.",
+                        "Manter a ordem de listas de pré-pedidos e reservas para clientes de cinco vendedoras."])])
+
+merrill = Company("Merrill Lynch &amp; Co., Inc., New York, NY",
+                  [Job("Associado de Verão, Pesquisas Globais",
+                       "Jun. 2008 &mdash; Ago. 2008",
+                       ["Modelar a frequência de pagamentos de títulos públicos hipotecários e calcular seus valores e perfis de risco.",
+                        "Testar a performance de um portfólio de futuros como um produto novo para substituir um portfólio tradicional de ações.",
+                        "Comparar o valor e volatilidade de futuros de petróleo relativo a ações de refinarias de grande porte."])])
+
+
+stux = Company("Stux Capital Management, LLC, New York, NY",
+               [Job("Estagiário, Investimentos em Ações",
+               "Feb. 2008 &mdash; Abr. 2008",
+               ["Consolidar, otimizar e checar erros em planilhas de rendas mensais.",
+                "Desenvolver uma interface visual que comunica em tempo real com um banco de dados para ver o lucro de um fundo de investimentos, aumentando a produtividade e minimizando erros."])])
+
+
+adair = Company("Adair Capital, LLC, New York, NY",
+                [Job("Estagiário, Fundo de Fundos de Cobertura",
+                "Set. 2007 &mdash; Jan. 2008",
+                ["Calcular níveis de risco entre fundos, correlacionando estratégias de fundos de cobertura com fatores de risco macroeconômicos para prever os lucros de fundos específicos.",
+                 "Desenvolver um GUI no Matlab que implementa um processo Brownian Bridge para medir a exposição de fundos de cobertura a índices de referência e detectar possíveis mudanças nas suas estratégias de investimento."])])
+
+                 
+COMPANIES = [pontual, merrill, stux, adair]
+
+
+stan_ug = Company("Graduação: Stanford University, Stanford, CA",
+                  [Job("Ciências Matemáticas e Computacionais",
+                       "Set. 2001 &mdash; Jun. 2005",
+                       ["Bachelor of Science, GPA 3.7/4.0"])])
+
+grad_school = Company("Pós-graduação: Stanford University, Stanford, CA",
+                      [Job("Ciência e Engenharia de Materiais",
+                           "Set. 2005 &mdash; Jun. 2007",
+                           ["Master of Science, GPA 3.6/4.0"])])
+
+nyu = Company("Pós-graduação: New York University, New York, NY",
+              [Job("Matemática Financeira",
+                   "Set. 2007 &mdash; Jan. 2009",
+                   ["Master of Science, GPA 3.4/4.0"])])
+
+SCHOOLS = [nyu, grad_school, stan_ug]
+
+
+materiais = Company("Departamento de Ciência e Engenharia de Materiais, Stanford University, Stanford, CA",
+                    [Job("Assistente de Pesquisas de Propriedades Mecânicas, Prof. R. H. Dauskardt",
+                         "Jun. 2005 &mdash; Fev. 2007",
+                         ["Construir simulações de fratura para prever a estabilidade de nanomateriais porosos inovadores.",
+                          "Otimizar scripts Matlab em programas C, acelerando cálculos que demoravam um dia para aproximadamente duas horas."])])
+
+
+carnegie = Company("Carnegie Institution for Science, Stanford, CA",
+                   [Job("Assistente de Pesquisas Genéticas, Dra. Susan S. Thayer",
+                        "Mar. 2004 &mdash; Jun. 2004",
+                        ["Realizar análises de regressão em SAS para identificar correlações estatisticamente significativas entre oito conjuntos de dados."])])
+
+
+ACADEMICA = [materiais, carnegie]
+
+
+criar = Company("Criar &amp; Criar Art Design, São Paulo, SP",
+                [Job("Web Designer",
+                     "Out. 2017 &mdash; Dez. 2017",
+                     ["Atualizar e manter o catálogo de produtos decorativos e sustentáveis no Blogspot."])])
+
+oca = Company("OCA Escola Cultural, Carapicuíba, SP",
+              [Job("Manutenção de Sistema, Ubuntu Linux",
+                   "Set. 2017",
+                   ["Configurar e instalar o gerenciador de bibliotecas Biblivre 5 dentro de um ambiente em nuvem."])])
+
+VOLUNTARIO = [criar, oca]
+
+
+polylexis = Company("Polylexis.com",
+                    [Job("Desenvolvedor Full-Stack",
+                         "Ago. 2019 &mdash; atual",
+                         ["Desenvolver e manter o site Django com vocabulário, testes e um mini-dicionário do grego moderno."])])
+
+
+hellokey = Company("Hello Key English",
+                   [Job("Designer e Desenvolvedor Full-Stack",
+                        "Out. 2019 &mdash; Nov. 2019",
+                        ["Gerenciar uma lista de alunos e um calendário de aulas particulares de inglês com o site Django.",
+                        "Desenhar uma interface otimizada para celular com o framework Bootstrap 4."])])
+
+
+PESSOAIS = [polylexis, hellokey]
+
+# Sections
+
+experiencia_profissional = ResumeSection("Experiência Profissional",
+                                         COMPANIES)
+
+formacao_academica = ResumeSection("Formação Acadêmica",
+                                   SCHOOLS)
+
+experiencia_academica = ResumeSection("Experiência Acadêmica", ACADEMICA)
+
+trabalho_voluntario = ResumeSection("Trabalho Voluntário", VOLUNTARIO)
+
+projetos_pessoais = ResumeSection("Projetos Pessoais", PESSOAIS)
+
+
+SECTIONS = [experiencia_profissional, formacao_academica, experiencia_academica, trabalho_voluntario, projetos_pessoais]
+
+
+def print_cv(fout, print_pdf=True):
+
+    try:
+        print("""
+    <div class="gent max-w-3xl mx-auto mt-8 text-gray-800 px-8">
+""", file=fout)
+
+        if print_pdf:
+            print("""
+        <!-- PDF icon -->
+        <div class="flex bg-white p-2 mx-auto">
+        <a href="resume/HeitorChang_pt.pdf">
+        <img src="img/pdficon.png" alt="PDF">
+        </a>
+        </div>
+
+        """, file=fout)
+
+        for section in SECTIONS:
+            if not print_pdf:
+                section_mt = "mt-6"
+                company_mt = "mt-3"
+                li_mt = "mt-1"
+            else:
+                section_mt = "mt-10"
+                company_mt = "mt-6"
+                li_mt = "mt-3"
+                
+                
+            print(f"""
+        <h2 class="text-xl text-teal-800 {section_mt} uppercase">{section.name}</h2>
+            """, file=fout)
+
+            for company in section.companies:
+                print(f"""
+            <h3 class="text-lg text-teal-700 {company_mt} border-solid border-b border-gray-500 uppercase">{company.name}</h3>
+                """, file=fout)
+
+                for job in company.jobs:
+                    print(f"""
+        <div class="flex items-stretch mt-2">
+            <div class="w-2/3 text-left italic">{job.title}</div>
+            <div class="w-1/3 text-right italic">{job.dates}</div>
+        </div>
+                    
+        <ul class="list-disc ml-8">
+        """, file=fout)
+
+                    for item in job.items:
+                        print(f"""
+            <li class="{li_mt}">{item}</li>
+                        """, file=fout)
+
+                    print("""
+        </ul>
+                    """, file=fout)
+
+
+        # Idiomas
+        IDIOMAS = (('Inglês', 'Fluente'),
+                   ('Mandarim', 'Intermediário'),
+                   ('Italiano', 'Intermediário'),
+                   ('Francês', 'Básico'),
+                   ('Espanhol', 'Básico'),
+                   ('Grego moderno', 'Básico'))
+        
+        print("""        <h2 class="text-xl text-teal-800 mt-8 uppercase">Idiomas</h2>""", file=fout)
+
+        print("""
+
+        <ul class="list-disc ml-8">
+        """, file=fout)
+
+        for idioma in IDIOMAS:
+            print(f"""
+          <li class="mt-1">
+            <div class="flex w-auto">
+              <div class="w-40">{idioma[0]}</div>
+              <div class="w-40 italic">{idioma[1]}</div>
+            </div>
+          </li>
+            """, file=fout)
+            
+        print("""
+        </ul>
+        """, file=fout)
+
+        # Programacao
+        LINGUAGENS = (('Python 3', 'Avançado'),
+                      ('HTML5', 'Avançado'),
+                      ('JavaScript (ES6)', 'Intermediário'),
+                      ('CSS3', 'Intermediário'),
+                      ('PHP 7', 'Intermediário'),
+                      ('Emacs Lisp', 'Básico'),
+                      ('MySQL', 'Básico'))
+                      
+        print("""        <h2 class="text-xl text-teal-800 mt-8 uppercase">Linguagens de Programação</h2>""", file=fout)
+
+        print("""
+
+        <ul class="list-disc ml-8">
+        """, file=fout)
+
+        for ling in LINGUAGENS:
+            print(f"""
+          <li class="mt-1">
+            <div class="flex w-auto">
+              <div class="w-40">{ling[0]}</div>
+              <div class="w-40 italic">{ling[1]}</div>
+            </div>
+          </li>
+            """, file=fout)
+            
+        print("""
+        </ul>
+        """, file=fout)
+
+
+        print("""    </div> <!-- CV gent font -->""", file=fout)
+
+    except Exception as e:
+        print("An exception occurred", e)
+
+
+if __name__ == "__main__":
+    with open("cv_en_print.html", 'w', encoding='utf-8') as fout:
+        print(open(CV_HEADER, encoding="utf-8").read(), file=fout)
+
+        print("""
+<div>
+    <div class="gent max-w-3xl text-2xl mx-auto mt-2 text-teal-900 px-8">
+        <h2 class="text-center uppercase">Heitor Chang</h2>
+    </div>
+
+    <div class="gent max-w-3xl mx-auto mt-2 text-gray-900 text-center px-8">
+
+        <div class="w-auto border-solid border-b border-gray-500 pb-2">
+        heitorpontual@gmail.com &nbsp;&diams;&nbsp; (11) 3312-8845 &nbsp;&diams;&nbsp; (11) 99907-4867 &nbsp;&diams;&nbsp; https://heitorchang.github.io/
+        </div>
+    </div>
+
+        """, file=fout)
+
+        print_cv(fout, False)
+
+        print("""
+        <div class="mb-8">&nbsp;</div>
+        </div></body></html> <!-- content -->""", file=fout)
+
+        print("Manually add page break in cv_print.html with")
+        print("""
+    <p style="page-break-after: always;">&nbsp;</p>
+    <p style="page-break-before: always;">&nbsp;</p>
+        """)
