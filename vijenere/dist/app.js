dd3b4ac8637f62d34185cf999a4c3e82b9934949
Heitor Chang <heitor.chang@tempook.com>
2022-02-24 13:54:06 -0300

    dist vijenere

---
+++ vijenere/dist/app.js
@@ -0,0 +1,36 @@
+function handleEncrypt() {
+  var input = document.getElementById("plaintext").value.trim();
+
+  var key = document.getElementById("key").value;
+  document.getElementById("key").value = key;
+
+  document.getElementById("ciphertext").value = vijenere.core.encrypt(input, key);
+}
+
+function handleDecrypt() {
+  var input = document.getElementById("ciphertext").value.trim();
+  var key = document.getElementById("key").value;
+  document.getElementById("key").value = key;
+
+  document.getElementById("plaintext").value = vijenere.core.decrypt(input, key);
+}
+
+document.getElementById("clearInputs").onclick = function() {
+  document.getElementById("plaintext").value = "";
+  document.getElementById("ciphertext").value = "";
+  document.getElementById("key").value = "";
+};
+
+document.getElementById("encryptBtn").onclick = function() {
+  handleEncrypt();
+  handleDecrypt();  // will clear skipped characters
+}
+
+document.getElementById("decryptBtn").onclick = function() {
+  handleDecrypt();
+}
+
+document.getElementById("copyResult").onclick = function() {
+  document.getElementById("ciphertext").select();
+  document.execCommand("copy");
+}
