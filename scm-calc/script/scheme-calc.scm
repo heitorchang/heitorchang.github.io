d3dd4d83e85998f80cd79b70b0a5b3be97bd37a5
Heitor Chang <heitor.chang@tempook.com>
2022-06-28 10:03:50 -0300

    single arg fn

--- scm-calc/script/scheme-calc.scm
+++ scm-calc/script/scheme-calc.scm
@@ -7,48 +7,73 @@
   (set-content! "#console"
                 (string-append (element-content (getelem "#console")) str "\n")))
 
-(print "BiwaScheme-Calc 0.8.1 num short, def
-(a)dd (s)ubtr (d)iv (m)ult (e x: sci.not.)
-(avg) (reset) Up/Down history, q: last val
+(print "BiwaScm-Calc 0.9.0 single f.x
+(a)dd (s)ubtr (d)iv (m)ult (e): sci.not.
+(avg) (reset) Up/Down hist, q: last val
 single number x: (a q x), (a) resets q
 (def x val)
 ")
 
-(define a +)
-(define s -)
-(define d /)
-(define m *)
+(define (a . vals)
+  (cond ((= (length vals) 0) 0)
+        ((= (length vals) 1) (+ q (car vals)))
+        (#t (apply + vals))))
+
+(define (s . vals)
+  (cond ((= (length vals) 0) 0)
+        ((= (length vals) 1) (- q (car vals)))
+        (#t (apply - vals))))
+
+(define (d . vals)
+  (cond ((= (length vals) 0) 1)
+        ((= (length vals) 1) (/ q (car vals)))
+        (#t (apply / vals))))
+
+(define (m . vals)
+  (cond ((= (length vals) 0) 1)
+        ((= (length vals) 1) (* q (car vals)))
+        (#t (apply * vals))))
+
 (define (avg . args)
   (/ (apply + args)
      (length args)))
 (define (reset) (js-eval "window.location.reload(true);"))
 (define q 0)
 
-(define (e x)
-  (print (js-eval (string-append "(" (number->string x) ").toExponential()"))))
+(define (e)
+  (print (js-eval (string-append "(" (number->string q) ").toExponential()"))))
 
 (define (set-input str)
   (js-eval (string-append "document.getElementById('replInput').value = '" str "';")))
 
 (define (repl-run)
   (let ((raw-input (regexp-replace-all "def " (element-content repl-input-elem) "define ")))
-    (let ((input-str (string-append "("
-                                    (if (number? (string->number raw-input))
-                                        (string-append "a q " raw-input)
-                                        raw-input)
-                                    ")")))
-      (if (not (string=? input-str "()"))
-          (begin
-            (set! *history-index* -1)
-            (set! *history* (cons (element-content repl-input-elem) *history*))
-            (print input-str)
-            (let ((exp-result (eval (read (open-input-string input-str)))))
-              (cond ((string? exp-result) (print exp-result))
-                    ((number? exp-result) (print (number->string exp-result)) (set! q exp-result))
-                    ((symbol? exp-result) (print (symbol->string exp-result))))
-              (print "")
-              (set-input "")
-              (js-eval "document.getElementById('console').scrollTop = document.getElementById('console').scrollHeight;")))))))
+    (if (string=? raw-input "q")
+        (begin
+          (set! *history-index* -1)
+          (set! *history* (cons raw-input *history*))
+          (print "q")
+          (print (number->string q))
+          (print "")
+          (set-input "")
+          (js-eval "document.getElementById('console').scrollTop = document.getElementById('console').scrollHeight;"))
+        (begin (let ((input-str (string-append "("
+                                               (if (number? (string->number raw-input))
+                                                   (string-append "a q " raw-input)
+                                                   raw-input)
+                                               ")")))
+                 (if (not (string=? input-str "()"))
+                     (begin
+                       (set! *history-index* -1)
+                       (set! *history* (cons (element-content repl-input-elem) *history*))
+                       (print input-str)
+                       (let ((exp-result (eval (read (open-input-string input-str)))))
+                         (cond ((string? exp-result) (print exp-result))
+                               ((number? exp-result) (print (number->string exp-result)) (set! q exp-result))
+                               ((symbol? exp-result) (print (symbol->string exp-result))))
+                         (print "")
+                         (set-input "")
+                         (js-eval "document.getElementById('console').scrollTop = document.getElementById('console').scrollHeight;")))))))))
 
 (add-handler! "#replRun" "click"
               (lambda (event)
@@ -93,6 +118,7 @@ single number x: (a q x), (a) resets q
                 (js-eval "document.getElementById('replInput').focus();")))
 
 (add-handler! "#showSci" "click"
-              (lambda (event) (e q)
+              (lambda (event) (e)
                       (print "")
+                      (js-eval "document.getElementById('console').scrollTop = document.getElementById('console').scrollHeight;")
                       (js-eval "document.getElementById('replInput').focus();")))
