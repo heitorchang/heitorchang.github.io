44a979aee39ee44b053f0b5a501b28bbce1f9c23
Heitor Chang <heitorpontual@gmail.com>
2023-04-07 22:11:56 -0300

    remove old lib

--- biblivre5-install/createold.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE biblivre3
-  WITH OWNER = biblivre
-       ENCODING = 'UTF8'
-       CONNECTION LIMIT = -1;
-
-CREATE DATABASE biblivre4_b3b_restore
-  WITH OWNER = biblivre
-       ENCODING = 'UTF8'
-       CONNECTION LIMIT = -1;
