c884aa6616ae8cdcebca4ff81dace42534490535
Heitor Chang <heitorpontual@gmail.com>
2023-05-07 14:52:47 -0300

    p 43

--- hcdocs/programming/scheme/biwascheme_files/raw/little-schemer.scm
+++ hcdocs/programming/scheme/biwascheme_files/raw/little-schemer.scm
@@ -10,3 +10,18 @@
       ((null? l) #t)
       ((atom? (car l)) (lat? (cdr l)))
       (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
+
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     ((eq? (car lat) a) (cdr lat))
+     (else (cons (car lat)
+                 (rember a (cdr lat)))))))
