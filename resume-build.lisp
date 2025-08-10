5c099777713967371985b852428144a4af6ead2e
Heitor Chang <hc@belgingur.is>
2025-08-10 17:38:44 -0300

    lisp resume

---
+++ resume-build.lisp
@@ -0,0 +1,101 @@
+;; Generate heitorchang.github.io Resume HTML
+
+(ql:quickload "cl-who")
+
+(defparameter *output-file* #P"/home/hcbel/code/heitorchang.github.io/resume.html")
+(defparameter *professional-items*
+  '((:company "Belgingur, LLC., Reykjavik, Iceland (Remote)"
+     :id "belgingur"
+     :roles ((:title "Senior Back-End Engineer"
+              :period "Jan. 2024 &ndash; present"
+              :responsibilities
+              ("Improved the flexibility of the Weather On Demand Python system by expanding the number of available configuration files."
+               "Completed a closest points linear regression model for weather forecasting."
+               "Converted scientific Fortran code that models volcanic activity to Python modules that leverage NumPy and NetCDF libraries."))))
+
+    (:company "Mediafly, Inc., Chicago, IL (Remote)"
+     :id "mediafly"
+     :roles ((:title "Senior Software Engineer, Data and Client Squad"
+              :period "Aug. 2022 &ndash; Feb. 2023"
+              :responsibilities
+              ("Extended a Python back-end service handling customer integration with Salesforce's API to allow the creation of new custom objects holding metadata."
+               "Improved the Python video call processing pipeline, filtering out unwanted blocks of silence."
+               "Corrected multiple occurrences of temporary files not being cleared and other data inconsistencies in the Python pipeline code."))))
+
+    (:company "Tempo OK Tecnologia em Meteorologia Ltd., São Paulo, SP"
+     :id "tempook"
+     :roles ((:title "Senior Full-Stack Developer"
+              :period "June 2020 &ndash; Aug. 2022"
+              :responsibilities
+              ("Built and administered the public-facing website with Django, including custom user management and authentication with a JWT extension."
+               "Built FastAPI endpoints to communicate with a PostgreSQL database using Google Cloud Functions and Cloud Run, leveraging Docker containers and Continuous Deployment."
+               "Created a geospatial dashboard containing satellite imagery overlays computed from NASA's scientific data and weather stations' historical data, obtained by accessing their public APIs."))))))
+
+(defun build-resume (professional-items)
+  (setf (cl-who:html-mode) :html5)
+  (setf cl-who:*attribute-quote-char* #\")
+
+  (with-open-file (out *output-file*
+                       :direction :output
+                       :if-exists :supersede)
+    (cl-who:with-html-output (out nil :prologue t :indent 4)
+      (:html
+       :lang "en"
+       (:head
+        (:meta :charset "utf-8")
+        (:meta :name "description" :content "Generated with resume-build.lisp")
+        (:meta :name "viewport" :content "width=device-width, initial-scale=1")
+        (:link :rel "stylesheet" :href "resume_maven_web.css")
+        (:title "Heitor Chang - Resume"))
+       (:body
+        (:h1 "Heitor Chang")
+
+        (:div :id "contact"
+              "heitorchang@gmail.com &bull; https://heitorchang.github.io/")
+
+        (:div :id "professional"
+              (:h2 "Professional Experience")
+              (:div
+               (dolist (pro-item professional-items)
+                 (cl-who:htm
+                  (:div
+                   (:h3
+                    :id (getf pro-item :id)
+                    (cl-who:str (getf pro-item :company)))
+                   (dolist (role (getf pro-item :roles))
+                     (cl-who:htm
+                      (:div
+                       (:div :class "prof-header-container"
+                             (:div :class "prof-header-title"
+                                   (cl-who:str (getf role :title)))
+                             (:div :class "prof-header-dates"
+                                   (cl-who:str (getf role :period))))
+                       (:ul
+                        (dolist (responsibility (getf role :responsibilities))
+                          (cl-who:htm
+                           (:li (cl-who:str responsibility)))))))))))))
+
+        (:div :id "education"
+              (:h2 "Education")
+              (:div
+               (:h3 :class "education" "Stanford University, Stanford, CA")
+               (:div :class "major" "M.S. in Materials Science and Engineering")
+               (:div :class "major" "B.S. in Applied Mathematics")))
+
+        (:div :id "languages"
+              (:h2 :class "skills" "Skills")
+              (:ul
+               (:li "Advanced knowledge of"
+                    (:strong "Python."))
+               (:li "Intermediate knowledge of"
+                    (:strong "JavaScript, ")
+                    (:strong "Django, ")
+                    (:strong "PostgreSQL, ")
+                    (:strong "Bash, ") "and"
+                    (:strong "Common Lisp."))
+               (:li "Fluent in"
+                    (:strong "English")
+                    "and"
+                    (:strong "Brazilian Portuguese.")
+                    "Intermediate knowledge of"
+                    (:strong "Italian.")))))))))
